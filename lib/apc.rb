d48e0d50576a8843c77c2dfb8814f6a3651221e7
Paul Thomas <paul+github@paulthomas.eu>
2013-10-10 14:17:55 +0100

    divide load by 10 as it is reported with 2.9 amps being 29

--- lib/apc.rb
+++ lib/apc.rb
@@ -109,7 +109,7 @@ class ApcSnmp
     loaddata = readValue(["rPDULoadStatusLoad.#{phase}",
                           "rPDULoadPhaseConfigOverloadThreshold.#{phase}",
                           "rPDULoadPhaseConfigNearOverloadThreshold.#{phase}"])
-    load['used'] = loaddata[0]
+    load['used'] = loaddata[0]/10 # Is reported as 2.9amps being 29
     load['max'] = loaddata[1]
     load['warn'] = loaddata[2]
     return load
